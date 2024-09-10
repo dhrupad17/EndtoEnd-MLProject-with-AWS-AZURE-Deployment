97f8b86632a9f0da2e0b54b1574fc95f7471173e
Dhrupad Chakraborty <dhrupadchakraborty123@gmail.com>
2024-09-10 11:58:08 +0530

    Implemented Model training with various Algorithms

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import os
 import dill
 import sys
+import pickle
+from sklearn.metrics import r2_score
+
 
 from src.exception import CustomException
 
@@ -17,3 +20,27 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]]=test_model_score
+
+            return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+    
